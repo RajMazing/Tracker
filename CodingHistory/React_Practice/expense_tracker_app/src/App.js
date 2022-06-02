7dfe16e5e2d8fcd58bb2092e061e98a757458a82
RajMazing <caseyraj92@gmail.com>
2022-06-01 20:44:16 -0500

    conflicts with git are resolved

---
+++ CodingHistory/React_Practice/expense_tracker_app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
