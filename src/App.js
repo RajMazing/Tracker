9bd40d4b126e12c0ddf18925d8a51085ae753a1a
RajMazing <caseyraj92@gmail.com>
2022-06-06 03:51:36 -0500

    Child-to-Parent Component communication from bottom to top

--- src/App.js
+++ src/App.js
@@ -27,11 +27,23 @@ const App = () => {
     },
   ];
 
+const addExpenseHandler = expense => {
+  console.log("in App.js")
+  console.log(expense)
+}
 
 
+
+  //return React.createElement(
+    // 'div',
+    //{},
+    //React.createElement('h2', {}, "Let's get started!")
+    //React.createElement(Expenses, {items: expenses})
+    //)
+  
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses}/>
       </div>
     
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   //first method for multiple states
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
@@ -69,7 +69,7 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData)
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
