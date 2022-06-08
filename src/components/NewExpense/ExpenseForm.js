2fb6972e22fb9b52589691bd68eff403de4c5e00
RajMazing <caseyraj92@gmail.com>
2022-06-08 15:51:44 -0500

    tracker now toggles between add new expense and cancel states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -98,7 +98,7 @@ const ExpenseForm = (props) => {
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input
+          <input 
             type="date"
             min="2019-01-01"
             max="2022-12-31"
@@ -108,6 +108,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
